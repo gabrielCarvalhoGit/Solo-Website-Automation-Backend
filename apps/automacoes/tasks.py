2bf1efdd6b1c0a890ad800f1bd67aa0f26f27ad2
gabrielCarvalhoGit <gabriel.carvalhogss@gmail.com>
2024-07-28 02:41:18 -0300

    Corrigindo processamento do arquivo ao salvar no banco utilizando celery.

--- apps/automacoes/tasks.py
+++ apps/automacoes/tasks.py
@@ -1,14 +1,17 @@
+import os
 from celery import shared_task
 from .models import Automacao
+from django.core.files import File
 
 
 @shared_task
 def criar_automacao(nome, descricao, arquivo):
     descricao = descricao if descricao else ''
+    file_name = os.path.basename(arquivo)
 
     with open(arquivo, 'rb') as f:
         automacao = Automacao(nome=nome, descricao=descricao)
-        automacao.arquivo.save(f.name, f)
+        automacao.arquivo.save(file_name, File(f))
 
         automacao.save()
 
@@ -18,8 +21,9 @@ def editar_automacao(id, nome, descricao, arquivo):
 
     automacao.nome = nome
     automacao.descricao = descricao if descricao else ''
+    file_name = os.path.basename(arquivo)
 
     with open(arquivo, 'rb') as f:
-        automacao.arquivo.save(f.name, f)
+        automacao.arquivo.save(file_name, File(f))
 
     automacao.save()
