4edfc37bc740808c833eabc2d3e30807e69f77af
gabrielCarvalhoGit <gabriel.carvalhogss@gmail.com>
2024-08-09 14:53:06 -0300

    Adicionando validação na API.

--- apps/accounts/api/views.py
+++ apps/accounts/api/views.py
@@ -1,4 +1,6 @@
+from rest_framework import serializers
 from rest_framework.response import Response
+from django.contrib.auth import authenticate
 from rest_framework.decorators import api_view
 
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -18,10 +20,19 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-
         token['email'] = user.email
 
         return token
+    
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(email=email, password=password)
+
+        if user is None:
+            raise serializers.ValidationError("Email ou senha inválidos.")
+        
+        return super().validate(attrs)
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
