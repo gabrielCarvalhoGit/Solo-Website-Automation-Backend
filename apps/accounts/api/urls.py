c4fda980a07970aefb74273c6cb79b7119e8f262
imrickss <henriqueluiz16022000@gmail.com>
2024-08-19 20:40:02 -0300

    url atualizada com endpoint de logout e refreshtoken

--- apps/accounts/api/urls.py
+++ apps/accounts/api/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from .views import get_routes, get_cookies_access_token, MyTokenObtainPairView
-
-from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-)
+from .views import MyTokenObtainPairView, refresh_access_token, get_cookies_access_token, get_routes, logout_user
 
 
 urlpatterns = [
     path('', get_routes),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', refresh_access_token, name='token_refresh'),
+    path('api/token/logout/', logout_user, name='logout_user'),
     path('token/get-cookies-token', get_cookies_access_token, name='get_cookies_token'),
+    
 ]
