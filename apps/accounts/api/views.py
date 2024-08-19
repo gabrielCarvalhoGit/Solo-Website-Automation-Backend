0cd2c96fc9477a289a943aadf6ea6a053901df98
imrickss <henriqueluiz16022000@gmail.com>
2024-08-19 20:43:12 -0300

    backend de logout e access token sendo adicionado pos requisiçao

--- apps/accounts/api/views.py
+++ apps/accounts/api/views.py
@@ -3,8 +3,7 @@ from rest_framework import serializers
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from rest_framework.decorators import api_view
-
-from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
 from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework_simplejwt.settings import api_settings
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -81,10 +80,42 @@ def get_cookies_access_token(request):
     
     return Response({'Access_token': access_token}, status=status.HTTP_200_OK)
 
+@api_view(['POST'])
+def refresh_access_token(request):
+    refresh_token = request.COOKIES.get('refresh_token')
+    if not refresh_token:
+        return Response({'detail': 'Refresh token não encontrado'}, status=status.HTTP_401_UNAUTHORIZED)
+
+    try:
+        refresh = RefreshToken(refresh_token)
+        access = refresh.access_token
+        response = Response({'access_token': str(access)}, status=status.HTTP_200_OK)
+        response.set_cookie(
+            key='access_token',
+            value=str(access),
+            httponly=True,
+            secure=True,
+            samesite='None'
+        )
+        return response
+    except TokenError:
+        return Response({'detail': 'Token inválido'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+@api_view(['POST'])
+def logout_user(request):
+    response = Response({'detail': 'Logout successful'}, status=status.HTTP_200_OK)
+    
+    # Remove os cookies de autenticação
+    response.delete_cookie('access_token')
+    response.delete_cookie('refresh_token')
+    
+    return response
+
 @api_view(['GET'])
 def get_routes(request):
     routes = [
         '/api/token',
+        '/api/token/logout',
         '/api/token/refresh',
         '/api/token/get-cookies-token'
     ]
