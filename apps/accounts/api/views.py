bbc6a544d58a8a89c7b17258e11b6d9f9111a159
Henrique Ataide <139990659+henilveira@users.noreply.github.com>
2024-08-20 17:44:05 -0300

    criando endpoints de logout e de acessar o refresh token

--- apps/accounts/api/views.py
+++ apps/accounts/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from rest_framework.decorators import api_view
 
-from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
 from rest_framework_simplejwt.settings import api_settings
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
@@ -68,6 +68,27 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
         return response
 
+@api_view(['POST'])
+def refresh_access_token(request):
+    refresh_token = request.COOKIES.get('refresh_token')
+    if not refresh_token:
+        return Response({'detail': 'Refresh token não encontrado'}, status=status.HTTP_401_UNAUTHORIZED)
+
+    try:
+        refresh = RefreshToken(refresh_token)
+        access = refresh.access_token
+        response = Response({'access_token': str(access)}, status=status.HTTP_200_OK)
+        response.set_cookie(
+            key='access_token',
+            value=str(access),
+            httponly=True,
+            secure=True,
+            samesite='None'
+        )
+        return response
+    except TokenError:
+        return Response({'detail': 'Token inválido'}, status=status.HTTP_401_UNAUTHORIZED)
+
 @api_view(['GET'])
 def get_user_session(request):
     access_token = request.COOKIES.get('access_token')
@@ -86,10 +107,22 @@ def get_user_session(request):
     except User.DoesNotExist:
         return Response({'detail': 'Usuário não encontrado.'}, status=status.HTTP_404_NOT_FOUND)
 
+@api_view(['POST'])
+def logout_user(request):
+    response = Response({'detail': 'Logout successful'}, status=status.HTTP_200_OK)
+    
+    # Remove os cookies de autenticação
+    response.delete_cookie('access_token')
+    response.delete_cookie('refresh_token')
+    
+    return response
+
+
 @api_view(['GET'])
 def get_routes(request):
     routes = [
         '/api/accounts/token',
+        '/api//accounts/token/logout',
         '/api/accounts/token/refresh',
         'api/accounts/get-user-session'
     ]
