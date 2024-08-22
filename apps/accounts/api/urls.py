5a6973b7809a65587f474fb2cf06809f3e3409d7
imrickss <henriqueluiz16022000@gmail.com>
2024-08-21 21:30:58 -0300

    atualizando sessao de usuario

--- apps/accounts/api/urls.py
+++ apps/accounts/api/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from .views import MyTokenObtainPairView, refresh_access_token, logout_user, get_user_session, get_routes, update_user_name
 
-
 urlpatterns = [
-    path('', get_routes),
+    path('', get_routes, name='api_root'),  # Rota principal para listar as rotas disponíveis
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/logout/', logout_user, name='logout_user'),
     path('token/refresh/', refresh_access_token, name='token_refresh'),
-    path('update-user-name/', update_user_name, name='update_user_name'),  # Rota nova para atualização do nome
-    path('token/get-user-session/', get_user_session, name='get_user')
-    
+    path('update-user-name/', update_user_name, name='update_user_name'),  # Rota para atualizar o nome do usuário
+    path('token/get-user-session/', get_user_session, name='get_user_session'),  # Rota para obter a sessão do usuário
 ]
